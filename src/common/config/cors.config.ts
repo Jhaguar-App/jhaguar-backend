4269806f71aecbea8d0084ab1d781f80810f5e73
Lucas Emanuel  Pereira Ribeiro <lucasemanuelpereiraribeiro@Lucass-MacBook-Air.local>
2025-11-27 21:04:20 -0300

    debug: allow all cors origins

--- src/common/config/cors.config.ts
+++ src/common/config/cors.config.ts
@@ -4,6 +4,9 @@ export function getAllowedOrigins(): string[] | boolean {
     return true;
   }
 
+  return true; // TEMPORARY DEBUG: Allow all origins
+  
+  /*
   const origins = [
     'https://jhaguar.com',
     'https://www.jhaguar.com',
@@ -17,6 +20,7 @@ export function getAllowedOrigins(): string[] | boolean {
   ].filter(Boolean) as string[];
 
   return origins;
+  */
 }
 
 export function getHttpCorsConfig() {
